efd858eaa220e0129dce7ced2ef35f7a8d9a4418
190201099 <190201099@kocaeli.edu.tr>
2022-03-17 12:51:42 +0300

    shows the route for given hours

--- static/google_maps.js
+++ static/google_maps.js
@@ -1,10 +1,7 @@
 let map;
 
-let distanceMatrix = [];
-let routeOrder = [];
-let routeOrderInt = [];
-//To get numbers in a string
-let regularExpression = /\d+/g;
+const bluemarker = "https://maps.google.com/mapfiles/kml/paddle/blu-circle.png"
+let markers = [];
 
 function initMap() {
 
@@ -13,6 +10,7 @@ function initMap() {
             zoom: 6,
         }
     );
+
     const mydata = JSON.parse(document.getElementById('mydata').textContent);
     // const new_data = mydata.replace("[","'")
     let mydataJson = JSON.parse(mydata);
@@ -23,7 +21,7 @@ function initMap() {
     // const new_data = mydata.replace("[","'")
     let user_id = JSON.parse(userId);
     user_id = JSON.parse(user_id);
-
+    console.log("user_id " + user_id);
 
     let currentDate = new Date();
     console.log(currentDate);
@@ -33,51 +31,102 @@ function initMap() {
 
     let car34indexes = [];
     let car386indexes = [];
+    let car292indexes = [];
+    let car246indexes = [];
     let car34index;
+    let car34LastIndex;
     let car386index;
+    let car386LastIndex;
+    let car292index;
+    let car292LastIndex;
+    let car246index;
+    let car246LastIndex;
 
     for (let i = 0; i < mydataJson.length; i++) {
         mydataJson[i].date_time = mydataJson[i].date_time.split(" ").slice(-1);
         if (mydataJson[i].user_id == user_id) {
+
             if (mydataJson[i].car_id == 34) {
                 if (mydataJson[i].date_time == currentTime) {
                     car34index = i;
-                } else {
-                    // console.log("currentTime " + currentTime)
-                    // console.log("date_time " + mydataJson[i].date_time)
                 }
                 car34indexes.push(i);
             } else if (mydataJson[i].car_id == 386) {
                 if (mydataJson[i].date_time == currentTime) {
                     // console.log("GİRDİ")
-
                     car386index = i;
-                    console.log("car386index " + car386index)
-                } else {
-                    // console.log("currentTime " + currentTime)
-                    // console.log("date_time " + mydataJson[i].date_time)
+                    // console.log("car386index " + car386index)
                 }
                 car386indexes.push(i);
+            } else if (mydataJson[i].car_id == 292) {
+                if (mydataJson[i].date_time == currentTime) {
+                    // console.log("GİRDİ")
+                    car292index = i;
+                    // console.log("car386index " + car386index)
+                }
+                car292indexes.push(i);
+            } else if (mydataJson[i].car_id == 246) {
+                if (mydataJson[i].date_time == currentTime) {
+                    // console.log("GİRDİ")
+                    car246index = i;
+                    // console.log("car386index " + car386index)
+                }
+                car246indexes.push(i);
             }
-        } else {
-            // console.log("mydata.user_id " + mydataJson[i].user_id)
-            // console.log("user_id " + user_id)
 
         }
     }
 
-    for (let i = car386index; (car386index-30) < i; i--) {
+    // car34LastIndex = car34indexes[-1];
+    // car386LastIndex = car386indexes[-1];
+    // car292LastIndex = car292indexes[-1];
+    // car246LastIndex = car292indexes[-1];
+
+    if (user_id == 2) {
+        last30Minutes(user_id, car34index, mydataJson, 0);
+        last30Minutes(user_id, car386index, mydataJson, 1);
+    } else if (user_id == 3) {
+        last30Minutes(user_id, car292index, mydataJson, 0);
+        last30Minutes(user_id, car246index, mydataJson, 1);
+    }
+
+    document.getElementById("submit").onclick = function () {
+        let startHour = document.getElementById("startHour").value;
+        let endHour = document.getElementById("endHour").value;
+        console.log(startHour)
+        console.log(endHour)
+        deleteMarkers();
+
+        if (user_id == 2) {
+            showRoute(user_id, car34indexes[0], car34indexes[-1], mydataJson, 0, startHour, endHour);
+            showRoute(user_id, car386indexes[0], car386indexes[-1], mydataJson, 1, startHour, endHour)
+        } else if (user_id == 3) {
+            showRoute(user_id, car292indexes[0], car292indexes[-1], mydataJson, 0, startHour, endHour);
+            showRoute(user_id, car246indexes[0], car246indexes[-1], mydataJson, 1, startHour, endHour);
+        }
+    }
+
+}
+
+// shows the route for the last 30 minutes
+function last30Minutes(user_id, carIndex, mydataJson, marker_id) {
+
+    for (let i = carIndex; (carIndex - 30) < i; i--) {
         console.log("GİRDİ")
         let coords = {lat: mydataJson[i].latitude, lng: mydataJson[i].longitude}
-        let marker = addMarker(coords)
+        let marker;
+        if (marker_id === 0) {
+            marker = addMarker(coords);
+        } else if (marker_id === 1) {
+            marker = addBlueMarker(coords);
+        }
+
         // console.log(typeof (mydataJson[0].latitude));
         let infowindow = new google.maps.InfoWindow({
             content: `lat:${coords.lat}, lng:${coords.lng},
                       date:${mydataJson[i].date_time},
                       car_id:${mydataJson[i].car_id}`
         });
-
-
         marker.addListener("click", () => {
             infowindow.open({
                 anchor: marker,
@@ -85,28 +134,35 @@ function initMap() {
                 shouldFocus: false,
             });
         });
-
     }
 
-    // for (let i = 0; i < 30; i++) {
-    //     // markers.push((mydataJson[i].latitude, mydataJson[i].longitude))
-    //     let coords = {lat: mydataJson[i].latitude, lng: mydataJson[i].longitude}
-    //     addMarker(coords)
-    //     // console.log(typeof (mydataJson[0].latitude));
-    //     // let infowindow = new google.maps.InfoWindow({
-    //     //     content: `lat:${coords.lat}, lng:${coords.lng},
-    //     //               date:${mydataJson[i].date_time},
-    //     //               car_id:${mydataJson[i].car_id}`
-    //     // });
-    //
-    //     // marker.addListener("click", () => {
-    //     //     infowindow.open({
-    //     //         anchor: marker,
-    //     //         map,
-    //     //         shouldFocus: false,
-    //     //     });
-    //     // });
-    // }
+}
+
+
+function showRoute(user_id, carFirstIndex, carLastIndex, mydataJson, marker_id, startHour, endHour) {
+    for (let i = (carFirstIndex + startHour * 60); i < (carFirstIndex + endHour * 60); i++) {
+        let coords = {lat: mydataJson[i].latitude, lng: mydataJson[i].longitude}
+        let marker;
+        if (marker_id === 0) {
+            marker = addMarker(coords);
+        } else if (marker_id === 1) {
+            marker = addBlueMarker(coords);
+        }
+
+        // console.log(typeof (mydataJson[0].latitude));
+        let infowindow = new google.maps.InfoWindow({
+            content: `lat:${coords.lat}, lng:${coords.lng},
+                      date:${mydataJson[i].date_time},
+                      car_id:${mydataJson[i].car_id}`
+        });
+        marker.addListener("click", () => {
+            infowindow.open({
+                anchor: marker,
+                map,
+                shouldFocus: false,
+            });
+        });
+    }
 
 }
 
@@ -117,9 +173,45 @@ function addMarker(coords) {
         position: coords,
         map: map
     });
+    markers.push(marker);
     return marker;
+}
+
+function addBlueMarker(coords) {
+    let marker = new google.maps.Marker({
+        position: coords,
+        icon: bluemarker,
+        map: map
+    });
+    markers.push(marker);
+    return marker;
+
+}
+
+
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  setMapOnAll(null);
+  markers = [];
 }
 
 function padTo2Digits(num) {
     return String(num).padStart(2, '0');
 }
+
+
+// function validate(ele) {
+//     var value = ele.value;
+//     var validation = ele.getAttribute("start-time")
+//     console.log(value + "  " + validation);
+//     return false;
+// }
